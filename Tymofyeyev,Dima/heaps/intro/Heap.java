5c8c5198f446a1d22fb5a19119e9cf756ff27e12
17dt0683 <17dt0683@HSS-101-11.westwindsor.k12.nj.us>
2016-04-07 07:46:53 -0400

    idk

--- Tymofyeyev,Dima/heaps/intro/Heap.java
+++ Tymofyeyev,Dima/heaps/intro/Heap.java
@@ -17,6 +17,15 @@ public class Heap{
 	}
 
     public void swapUp(int bot){
+    	while(bot > 0){
+    		int parent = (bot-1)/2;
+    		if (list.get(parent) < list.get(bot)){
+    			swap(parent, bot);
+    			bot = parent;
+    		}
+    		else
+    			bot = 0;
+    	}
     }
 
 	public void remove( ){
@@ -26,14 +35,43 @@ public class Heap{
 	}
 
 	public void swapDown(int top){
+		int root = 0;
+		int max = -1;
+		while(root < top){
+			int left = root *2;
+			int right = root*2+1;
+			
+			if(list.get(left) != null){
+				if (list.get(right) != null){
+					if( list.get(left) > list.get(right))
+						max = left;
+					else 
+						max = right;
+				}
+				else
+					max = left;			
+			}
+			else
+				root = top;
+			
+			if( list.get(max) > list.get(root)){
+				swap(max, root);
+				root = max;
+			}
+		}
 	}
 	
 	private void swap(int start, int finish){	
+		int temp = list.get(start);
+		list.set(start, list.get(finish));
+		list.set(finish, temp);
 	}
 
 	public void print(){
 		out.println("\n\nPRINTING THE HEAP!\n\n");
 		out.println();
+		for (int i  = 0; i < list.size(); i ++)
+			System.out.print(list.get(i)+ " ");
 	}
 
 	public String toString(){
