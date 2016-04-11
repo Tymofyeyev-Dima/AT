f6213b4b8944e4b32f83c4aec6a4887f9bd5757d
tymofyeyev_dima <tymofyeyev_dima@10.250.24.192>
2016-04-11 10:17:42 -0400

    done with monster class

--- Tymofyeyev,Dima/heaps/monsters/Monster.java
+++ Tymofyeyev,Dima/heaps/monsters/Monster.java
@@ -1,5 +1,64 @@
 package monsters;
 
-public class Monster {
+public class Monster{
 
+	private int ht, wt, age;
+	
+	public Monster(int height, int weight, int age){
+		ht = height;
+		wt = weight;
+		this.age = age;
+	}
+	
+	public void setWeight(int wt){
+		this.wt = wt;
+	}
+	
+	public void setHeight(int ht){
+		this.ht = ht;
+	}
+	
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+	public Object clone(){
+		return new Monster(ht, wt, age);
+	}
+
+	public int getHeight() {
+		return ht;
+	}
+
+	public int getWeight() {
+		return wt;
+	}
+
+	public int getAge() {
+		return age;
+	}
+	
+	public boolean equals(Object o){
+		return ((Monster) o).getWeight() == wt && ((Monster) o).getHeight() == ht && ((Monster) o).getAge() == age;
+	}
+
+	public int compareTo(Monster other) {
+		if(equals(other))
+			return 0;
+		else if (other.getHeight()!=ht)
+			return other.getHeight()-ht;
+		else if (other.getWeight()!=wt)
+			return other.getWeight()-wt;
+		else
+			return -1;
+		
+	}
+	
+	public String toString(){
+		return "Height: "+ht+"| Weight: "+wt+"| Age: "+age;
+	}
+	
+	
+	
+	
 }
