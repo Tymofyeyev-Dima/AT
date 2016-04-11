b7345879f2a33f47f07c09be6426971e72d679ec
17dt0683 <17dt0683@HSS-101-11.westwindsor.k12.nj.us>
2016-04-11 10:02:04 -0400

    initial commit

---
+++ Tymofyeyev,Dima/heaps/monsters/YourPQ.java
@@ -0,0 +1,50 @@
+package monsters;
+
+
+
+import java.util.Queue;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.PriorityQueue;
+
+public class YourPQ
+{
+	private Queue pQueue;
+
+	public YourPQ()
+	{
+
+
+	}
+
+	public void add(Object obj)
+	{
+
+
+	}
+	
+	public Object getMin()
+	{
+		return "";
+	}
+	
+	public Object removeMin()
+	{
+		return "";
+	}
+	
+	public String getNaturalOrder()
+	{
+		String output="";
+
+
+
+		return output;		
+	}
+
+	//write a toString method
+	
+	
+}
