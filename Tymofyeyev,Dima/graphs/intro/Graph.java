6d4950ce9ab28a46ef72bbfc8598b6b3a2e047d1
17dt0683 <17dt0683@HSS-101-11.westwindsor.k12.nj.us>
2016-04-14 09:21:06 -0400

    intial commit

---
+++ Tymofyeyev,Dima/graphs/intro/Graph.java
@@ -0,0 +1,28 @@
+package intro;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Graph
+{
+	private TreeMap<String, String> map;
+	private boolean yahOrNay;
+
+	public Graph(String line)
+	{
+	}
+
+	public boolean contains(String letter)
+	{
+	   return true;
+	}
+
+	public void check(String first, String second, String placesUsed)
+	{
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+}
