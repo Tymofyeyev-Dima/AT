7faf276819a445887e72ce6de2a65b8e644472da
17dt0683 <17dt0683@HSS-101-11.westwindsor.k12.nj.us>
2016-04-26 10:14:06 -0400

    done with graphs 1

--- Tymofyeyev,Dima/graphs/intro/Graph.java
+++ Tymofyeyev,Dima/graphs/intro/Graph.java
@@ -1,28 +1,57 @@
 package intro;
 
-import java.util.Map;
 import java.util.TreeMap;
 
-public class Graph
-{
+public class Graph {
 	private TreeMap<String, String> map;
-	private boolean yahOrNay;
+	public boolean yahOrNay;
 
-	public Graph(String line)
-	{
+	public Graph(String line) {
+		map = new TreeMap<String, String>();
+		yahOrNay = false;
+		String[] pair = line.split(" ");
+		for (int x = 0; x < pair.length; x++) {
+			if (map.isEmpty()) {
+				map.put(pair[x].substring(0, 1), pair[x].substring(1));
+				map.put(pair[x].substring(1), pair[x].substring(0, 1));
+			} else {
+				String first = pair[x].substring(0, 1);
+				String second = pair[x].substring(1);
+				if (containsMap(first)) {
+					map.put(first, map.get(first) + second);
+				} else
+					map.put(first, second);
+
+				if (containsMap(second)) {
+					map.put(second, map.get(second) + first);
+				} else
+					map.put(second, first);
+			}
+		}
+
+		//System.out.println(map);
 	}
 
-	public boolean contains(String letter)
-	{
-	   return true;
+	public boolean containsMap(String letter) {
+		return map.containsKey(letter);
 	}
 
-	public void check(String first, String second, String placesUsed)
-	{
+	public void check(String first, String second, String placesUsed) {
+		if (!yahOrNay) {
+			if (map.get(first).contains(second))
+				yahOrNay = true;
+			else {
+				String temp = map.get(first);
+				for (int i = 0; i < temp.length(); i++)
+					if (!placesUsed.contains(temp.substring(i, i + 1))) {
+						placesUsed += temp.substring(i, i + 1);
+						check(temp.substring(i, i + 1), second, placesUsed);
+					}
+			}
+		}
 	}
 
-	public String toString()
-	{
+	public String toString() {
 		return "";
 	}
 }
--- Tymofyeyev,Dima/graphs/intro/GraphRunner.java
+++ Tymofyeyev,Dima/graphs/intro/GraphRunner.java
@@ -9,12 +9,14 @@ public class GraphRunner
 {
 	public static void main( String[] args ) throws IOException
 	{
-		Scanner file = new Scanner(new File("graph.dat"));
-		int howManyTimes = file.nextInt();
-		file.nextLine();
-		for(int x=0; x<howManyTimes; x++)
-		{
-			//add code here
+		Scanner file = new Scanner(new File("H:\\git\\AT\\Tymofyeyev,Dima\\graphs\\intro\\graph1.dat"));
+		int howManyTimes = Integer.parseInt(file.nextLine());
+		for(int x=0; x<howManyTimes; x++){
+			Graph g = new Graph(file.nextLine());
+			String check = file.nextLine();
+			g.check(check.substring(0,1), check.substring(1), check.substring(0,1));
+			System.out.println(check.substring(0,1)+" connects to " + check.substring(1) +" "+g.yahOrNay);
 		}
+		file.close();
 	}
 }
