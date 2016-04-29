7cc2078027c31bc1f268fe8559301adcfc8f4bac
17dt0683 <17dt0683@HSS-101-11.westwindsor.k12.nj.us>
2016-04-29 08:16:36 -0400

    done with shortest path

--- Tymofyeyev,Dima/graphs/shortestPath/ShortestPathGraph.java
+++ Tymofyeyev,Dima/graphs/shortestPath/ShortestPathGraph.java
@@ -1,35 +1,62 @@
 package shortestPath;
 
-
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
 import java.util.TreeMap;
-import java.util.Scanner;
-import static java.lang.System.*;
 
-public class ShortestPathGraph
-{
+public class ShortestPathGraph {
 	private TreeMap<String, String> map;
-	private boolean yayOrNay;
-	private int shortest;
+	public boolean yayOrNay;
+	public int shortest;
 
-	public ShortestPathGraph(String line)
-	{
+	public ShortestPathGraph(String line) {
+		map = new TreeMap<String, String>();
+		yayOrNay = false;
+		shortest = 0;
+		
+		String[] pair = line.split(" ");
+		for (int x = 0; x < pair.length; x++) {
+			if (map.isEmpty()) {
+				map.put(pair[x].substring(0, 1), pair[x].substring(1));
+				map.put(pair[x].substring(1), pair[x].substring(0, 1));
+			} 
+			else {
+				String first = pair[x].substring(0, 1);
+				String second = pair[x].substring(1);
+				if (contains(first))
+					map.put(first, map.get(first) + second);
+				else
+					map.put(first, second);
+
+				if (contains(second))
+					map.put(second, map.get(second) + first);
+				else
+					map.put(second, first);
+			}
+		}
 	}
 
-	public boolean contains(String letter)
-	{
-		return true;
+	public boolean contains(String letter) {
+		return map.containsKey(letter);
 	}
 
-	public void check(String first, String second, String placesUsed, int steps)
-	{
+	public void check(String first, String second, String placesUsed, int steps) {
+		if (!yayOrNay) {
+			if (map.get(first).contains(second)) {
+				if(shortest == 0 || steps < shortest)
+					shortest = steps;
+				yayOrNay = true;
+			} 
+			else {
+				String temp = map.get(first);
+				for (int i = 0; i < temp.length(); i++)
+					if (!placesUsed.contains(temp.substring(i, i + 1))) {
+						placesUsed += temp.substring(i, i + 1);
+						check(temp.substring(i, i + 1), second, placesUsed, steps + 1);
+					}			
+			}
+		}
 	}
 
-	public String toString()
-	{
+	public String toString() {
 		return "";
 	}
 }
--- Tymofyeyev,Dima/graphs/shortestPath/ShortestPathGraphRunner.java
+++ Tymofyeyev,Dima/graphs/shortestPath/ShortestPathGraphRunner.java
@@ -1,15 +1,22 @@
 package shortestPath;
 
-
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
-import static java.lang.System.*;
 
-public class ShortestPathGraphRunner
-{
-	public static void main( String[] args ) throws IOException
-	{
+public class ShortestPathGraphRunner{
+	public static void main( String[] args ) throws IOException{
+		Scanner file = new Scanner(new File("H:\\git\\AT\\Tymofyeyev,Dima\\graphs\\shortestPath\\test_data"));
+		int howManyTimes = Integer.parseInt(file.nextLine());
+		for(int x=0; x<howManyTimes; x++){
+			ShortestPathGraph g = new ShortestPathGraph(file.nextLine());
+			String check = file.nextLine();
+			g.check(check.substring(0,1), check.substring(1), check.substring(0,1), 1);
+			if(g.yayOrNay)
+				System.out.println(check.substring(0,1)+" connects to " + check.substring(1) +" | "+g.yayOrNay+" in "+g.shortest+" steps");
+			else
+				System.out.println(check.substring(0,1)+" connects to " + check.substring(1) +" | "+g.yayOrNay);
+		}
+		file.close();
 	}
 }
