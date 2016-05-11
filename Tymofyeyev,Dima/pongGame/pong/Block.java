a22fd006caf7a00dfc2b4fac770435c57d66f6fb
17dt0683 <17dt0683@HSS-101-11.westwindsor.k12.nj.us>
2016-05-11 08:31:42 -0400

    need to finish draw() method

--- Tymofyeyev,Dima/pongGame/pong/Block.java
+++ Tymofyeyev,Dima/pongGame/pong/Block.java
@@ -4,66 +4,102 @@ import java.awt.Graphics;
 
 public class Block implements Locatable
 {
-	private int xPos;
-	private int yPos;
-	private int width;
-	private int height;
-
+	private int xPos, yPos, width, height;
 	private Color color;
-
-	public Block()
-	{
-
-
+	
+	public Block(){
+		xPos = 0; 
+		yPos = 0;
+		width = 0;
+		height = 0;
+		color = Color.BLACK;
 	}
 
-	//add other Block constructors - x , y , width, height, color
+	public Block (int xPos, int yPos, int width, int height){
+		this.xPos = xPos; 
+		this.yPos = yPos;
+		this.width = width;
+		this.height = height;
+		this.color = Color.BLACK;
+	}
 	
+	public Block(int xPos, int yPos, int width, int height, Color color){
+		this.xPos = xPos; 
+		this.yPos = yPos;
+		this.width = width;
+		this.height = height;
+		this.color = color;
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-   //add the other set methods
-   
 
-   public void setColor(Color col)
-   {
-
-
-   }
-
-   public void draw(Graphics window)
-   {
-   	//uncomment after you write the set and get methods
-      //window.setColor(color);
-      //window.fillRect(getX(), getY(), getWidth(), getHeight());
-   }
-
-   public void draw(Graphics window, Color col)
-   {
-
-
+   public void setWidth(int w){
+	   width = w;
    }
    
-	public boolean equals(Object obj)
-	{
+   public void setHeight(int h){
+	   height = h;
+   }
+   
+   public void setX(int x){
+	   xPos = x;
+   }
+   
+   public void setY(int y){
+	   yPos = y;
+   }
+   
+   public void setPos(int x, int y){
+	   xPos = x;
+	   yPos = y;
+   }
 
+   public void setColor(Color col){
+	   color = col;
+   }
 
+	public int getWidth() {
+		return width;
+	}
 
+	public int getHeight() {
+		return height;
+	}
 
-		return false;
-	}   
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getX(){
+		return xPos;
+	}
+	
+	public int getY(){
+		return yPos;
+	}
+	
+	public void draw(Graphics window) {
+		window.setColor(color);
+		window.fillRect(xPos, yPos, width, height);
+	}
 
-   //add the other get methods
-    
+	public void draw(Graphics window, Color col) {
+		window.setColor(col);
+		window.fillRect(xPos, yPos, width, height);
+	}
 
-   //add a toString() method  - x , y , width, height, color
+	public boolean equals(Object obj) {
+		if (xPos == ((Block) obj).getX() && yPos == ((Block) obj).getY() && 
+				height == ((Block) obj).getHeight() && width == ((Block) obj).getWidth())
+			return true;
+		else
+			return false;
+	}   	
+
+	
+	public String toString(){
+		return "("+xPos+", "+yPos+")  w: "+width+"  h: "+height+"  c: "+color;
+	}
 }
+
+
+
